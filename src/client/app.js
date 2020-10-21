2d73d28fb913c57e2a9ba73ec98dd9aa7bd9758a
Vi <rymsha.viktor@gmail.com>
2020-10-21 14:14:50 +0300

    feat: add props

--- src/client/app.js
+++ src/client/app.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    const names = this.props.data;
+    const secondPerson = names[1]
+
     return (
       <div>
-        Hello World
+        Hello {secondPerson}
       </div>
     );
   }
--- src/server/index.js
+++ src/server/index.js
@@ -3,34 +3,36 @@ import cors from "cors";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import App from '../client/app';
+import serialize from "serialize-javascript";
 
 const app = express();
 
 app.use(cors());
-
-// We're going to serve up the public
-// folder since that's where our
-// client bundle.js file will end up.
 app.use(express.static("public"));
 
 app.get("*", (req, res, next) => {
-    const markup = renderToString(
-      <App />
-    );
-  
-    res.send(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>SSR with RR</title>
-        </head>
-  
-        <body>
-          <div id="app">${markup}</div>
-        </body>
-      </html>
-    `);
-  })
+      
+  const names = ['Tyler', 'Petr', 'Michail']
+
+  const markup = renderToString(
+    <App data={names}/>
+  );
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>SSR with RR</title>
+      </head>
+
+      <body>
+        <div id="app">${markup}</div>
+      </body>
+      <script src="/bundle.js" defer></script>
+      <script>window.__INITIAL_DATA__ = ${serialize(names)}</script>
+    </html>
+  `);
+})
 
 app.listen(3000, () => {
   console.log(`Server is listening on port: 3000`);
